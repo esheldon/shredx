7f220eac7f08c2dc992c336405cc5b58c1151268
Erin Sheldon <erin.sheldon@gmail.com>
2019-09-14 07:35:17 -0400

    skeleton of loader test

--- shredx/tests/test_loader.py
+++ shredx/tests/test_loader.py
@@ -8,3 +8,40 @@ TODO
       shredder, with a fake psfex file from anywhere.
       will need to fake the wcs header
 """
+
+import numpy as np
+import shredder
+import shredx
+from tempfile import TemporaryDirectory
+
+
+def _write_simulated_files(tmpdir, seed):
+    rng = np.random.RandomState(seed)
+    sim = shredder.Sim(rng=rng)
+
+    mbobs = sim()
+
+    # run sep to get catalog and seg map, write
+    # to files
+
+
+def test_loader_smoke(seed):
+
+    with TemporaryDirectory() as tmpdir:
+        image_files, seg_file, cat_file = _write_simulated_files(tmpdir, seed)
+
+        nband = len(image_files)
+        psf_files = ['something']*nband
+
+        loader = shredx.Loader(
+            image_files=image_files,
+            psf_files=psf_files,
+            seg_file=seg_file,
+            cat_file=cat_file,
+            coord_offset=0,
+        )
+
+        # or whatever
+        numbers = [1, 2, 3]
+
+        mbobs = loader.get_mbobs(numbers)
