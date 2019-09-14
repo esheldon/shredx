fa6812f68a72938b2bc4e259e2bb7a9c9697c43b
Erin Sheldon <erin.sheldon@gmail.com>
2019-09-14 07:12:30 -0400

    added empty test

---
+++ shredx/tests/test_loader.py
@@ -0,0 +1,10 @@
+"""
+TODO
+    - example psfex file
+    - example g,r,i,z image files with weight maps
+    - example seg file
+    - example cat file
+    - we can generate these using the simulation in
+      shredder, with a fake psfex file from anywhere.
+      will need to fake the wcs header
+"""
