190abaaa4f2d684d0c78b105ebf95eb6933c1e90
Erin Sheldon <erin.sheldon@gmail.com>
2019-09-16 06:15:44 -0400

    working on basic test of loader

--- shredx/tests/test_loader.py
+++ shredx/tests/test_loader.py
@@ -1,18 +1,10 @@
-"""
-TODO
-    - example psfex file
-    - example g,r,i,z image files with weight maps
-    - example seg file
-    - example cat file
-    - we can generate these using the simulation in
-      shredder, with a fake psfex file from anywhere.
-      will need to fake the wcs header
-"""
-
+import logging
 import numpy as np
 import pytest
 import shredx
 from tempfile import TemporaryDirectory
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.mark.parametrize('seed', [6419, 491])
@@ -40,3 +32,42 @@ def test_loader_smoke(seed):
         numbers = [1, 2, 3]
 
         mbobs = loader.get_mbobs(numbers)  # noqa
+
+
+@pytest.mark.parametrize('seed', [2510, 21064])
+def test_loader(seed):
+    """
+    test we can run the fof finder and extract the group
+    """
+
+    import fofx
+    # import esutil
+
+    shredx.setup_logging('debug')
+    rng = np.random.RandomState(seed)
+
+    with TemporaryDirectory() as tmpdir:
+        image_files, psfs, seg_file, cat_file = \
+            shredx.sim.get_simulated_files(tmpdir, rng=rng)
+
+        loader = shredx.Loader(
+            image_files=image_files,
+            psf_files=psfs,
+            seg_file=seg_file,
+            cat_file=cat_file,
+            coord_offset=0,
+        )
+
+        fofs = fofx.get_fofs(loader.seg)
+
+        fof_ids = np.unique(fofs['fof_id'])
+
+        # esutil.numpy_util.aprint(fofs)
+
+        for fof_id in fof_ids:
+            logger.info('processing fof: %d' % fof_id)
+            w, = np.where(fofs['fof_id'] == fof_id)
+            numbers = 1+w
+
+            logger.info('ids: %s numbers: %s' % (str(w), str(numbers)))
+            mbobs = loader.get_mbobs(numbers)  # noqa
