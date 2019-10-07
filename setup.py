d796464c70e37b94c5dde0af8b15124f28bb9195
Erin Sheldon <erin.sheldon@gmail.com>
2019-10-07 10:36:27 -0400

    adding version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ scripts = [s for s in scripts if '~' not in s]
 
 setup(
     name='shredx',
-    version='v0.1.0',
+    version='v0.9.0',
     description=('Run the shredder image deblender on '
                  'images processed with sextractor'),
     license='GPL',
--- shredx/__init__.py
+++ shredx/__init__.py
@@ -1,3 +1,4 @@
+from .version import __version__
 from . import loader
 from .loader import Loader
 
